b249a607652223e4183eb6a29cef692f715cfba4
Nihir Prajapati <nihir12121@gmail.com>
2026-09-12 14:37:42 +0530

    Add two more owner emails to admin allowlist
    sartanparanihir@gmail.com and ferved69@gmail.com added to ADMIN_EMAILS.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
 export const APP_NAME = 'Hirely'
 
-export const ADMIN_EMAILS = ['nihir12121@gmail.com']
+export const ADMIN_EMAILS = [
+  'nihir12121@gmail.com',
+  'sartanparanihir@gmail.com',
+  'ferved69@gmail.com',
+]
 
 export const MAX_FILE_SIZE = 10 * 1024 * 1024 // 10 MB
 export const MIN_TEXT_LENGTH = 50
